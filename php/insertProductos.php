2020fe6cc2000a19f6e26a1bdb3b9db4661f5a2d
Oscar0189 <ofmb89@gmail.com>
2025-04-10 19:53:14 +0200

    array switches insertProductos.php
    Se ha añadido array de switches

--- php/insertProductos.php
+++ php/insertProductos.php
@@ -1,200 +1,67 @@
 <?php
 $productos = [
     [
-        'nombre' => 'NocdMON',
-        'descripcion' => 'Hosting de servicio backend en PHP',
-        'precio' => 27.54,
-        'img' => 'img/productos/web/phpHosting.jpg',
-        'categoria' => 'Web'
+        'nombre' => 'Cisco IE-3300',
+        'descripcion' => 'Conmutador Ethernet Cisco Catalyst IE3300 IE-3300 con 8 puertos gestionable y 2 ranuras SFP. Los switches de Cisco cuentan con la escalabilidad necesaria para satisfacer las necesidades de redes de todos los tamaños.',
+        'precio' => 2345.97,
+        'img' => 'img/productos/switches/1.webp',
+        'categoria' => 'Switches'
     ],
     [
-        'nombre' => 'JoSting',
-        'descripcion' => 'Hosting de frontend para frameworks en javascript como react o vue',
-        'precio' => 32.65,
-        'img' => 'img/productos/web/JSHosting.jpg',
-        'categoria' => 'Web'
+        'nombre' => 'Cisco Business CBS110-16PP',
+        'descripcion' => 'Forman parte del portafolio de redes Cisco Business, son una familia de switches asequibles que brindan conectividad Gigabit Ethernet para la red de su pequeña empresa.',
+        'precio' => 257.01,
+        'img' => 'img/productos/switches/2.jpg',
+        'categoria' => 'Switches'
     ],
     [
-        'nombre' => 'FrontWind',
-        'descripcion' => 'Plantillas para componentes en HTML y CSS',
-        'precio' => 52.44,
-        'img' => 'img/productos/web/plantillas.jpg',
-        'categoria' => 'Web'
+        'nombre' => 'Cisco Catalyst C1000-24P-4G-L',
+        'descripcion' => 'Los switches Cisco ® Catalyst ® de la serie 1000 están fijos y son configurables Gigabit Ethernet y Fast Ethernet de Capa 2 de clase empresarial de conmutadores diseñados para las pequeñas empresas y sucursales.',
+        'precio' => 990.63,
+        'img' => 'img/productos/switches/3.webp',
+        'categoria' => 'Switches'
     ],
     [
-        'nombre' => 'TypeForm',
-        'descripcion' => 'Formularios prehechos personalizables para recoger datos',
-        'precio' => 12.55,
-        'img' => 'img/productos/web/forms.jpg',
-        'categoria' => 'Web'
+        'nombre' => 'Cisco CBS110-16T-EU',
+        'descripcion' => 'Estos conmutadores de nivel de entrada asequibles, plug-and-play, proporcionan conmutación Gigabit Ethernet con características como Power over Ethernet (PoE), eficiencia energética y priorización de tráfico para una transformación digital perfecta.',
+        'precio' => 156.80,
+        'img' => 'img/productos/switches/4.webp',
+        'categoria' => 'Switches'
     ],
     [
-        'nombre' => 'Nodisocord',
-        'descripcion' => 'Libreria para servicio de hosting de canales de videollamada privados para tu web',
-        'precio' => 299.00,
-        'img' => 'img/productos/web/notDiscord.jpg',
-        'categoria' => 'Web'
+        'nombre' => 'Cisco C1000-48P-4X-L',
+        'descripcion' => 'Este switch de red sencillo, flexible y seguro es ideal para las implementaciones fundamentales de IoT (Internet de las cosas). Funciona con el software Cisco IOS y cuenta con una CLI (interfaz de línea de comandos) y una interfaz de usuario web intuitiva.',
+        'precio' => 2646.00,
+        'img' => 'img/productos/switches/5.webp',
+        'categoria' => 'Switches'
     ],
     [
-        'nombre' => 'Paloma',
-        'descripcion' => 'Libreria para servicio de hosting de chats privados o publicos para tu web',
-        'precio' => 29.99,
-        'img' => 'img/productos/web/chats.jpg',
-        'categoria' => 'Web'
+        'nombre' => 'Cisco SF110-16',
+        'descripcion' => 'Proporcionan todas las funciones, además de capacidad de expansión y la protección de la inversión que cabe esperar de Cisco, sin necesidad de software de instalación y sin nada que configurar.',
+        'precio' => 117.57,
+        'img' => 'img/productos/switches/6.webp',
+        'categoria' => 'Switches'
     ],
     [
-        'nombre' => 'Nomaps',
-        'descripcion' => 'Libreria para crear un servicio de geolocalización en tu web',
-        'precio' => 42.42,
-        'img' => 'img/productos/web/notMaps.jpg',
-        'categoria' => 'Web'
+        'nombre' => 'Cisco SF110-24',
+        'descripcion' => 'Proporcionan fiabilidad y conectividad de red básica para pequeñas empresas, sin complejidad. Solo tiene que enchufarlo, conectar los equipos informáticos y otros dispositivos de empresa, y comenzar a trabajar.',
+        'precio' => 59.99,
+        'img' => 'img/productos/switches/7.webp',
+        'categoria' => 'Switches'
     ],
     [
-        'nombre' => 'Nostripe',
-        'descripcion' => 'Servicio de pasarela de pago super segura para tu eshop',
-        'precio' => 33.78,
-        'img' => 'img/productos/web/notStripe.jpg',
-        'categoria' => 'Web'
+        'nombre' => 'Cisco SG110D-05',
+        'descripcion' => 'Los switches no gestionados SG110D-05 de Cisco proporcionan fiabilidad y conectividad de red básica para pequeñas empresas por un precio asequible y sin apenas complejidad.',
+        'precio' => 95.16,
+        'img' => 'img/productos/switches/8.webp',
+        'categoria' => 'Switches'
     ],
     [
-        'nombre' => 'YourSQL',
-        'descripcion' => 'Servicio de hosting de bases de datos SQL',
-        'precio' => 27.65,
-        'img' => 'img/productos/web/yourSQL.jpg',
-        'categoria' => 'Web'
-    ],
-    [
-        "nombre" => "Redes locales",
-        "descripcion" => "Diseño, implementación y mantenimiento de redes locales para facilitar la comunicación y transferencia de datos dentro de una empresa.",
-        "precio" => 300.99,
-        "img" => "img/productos/redes/config_redes_locales.jpg",
-        "categoria" => "Redes"
-    ],
-    [
-        "nombre" => "Redes Wi-Fi",
-        "descripcion" => "Configuración, optimización y monitoreo de redes Wi-Fi, asegurando cobertura y velocidad en todos los puntos de la red.",
-        "precio" => 350.99,
-        "img" => "img/productos/redes/redes_inalambricas.jpg",
-        "categoria" => "Redes"
-    ],
-    [
-        "nombre" => "Gestión de redes",
-        "descripcion" => "Administración de la conexión a internet de la empresa, incluyendo la configuración de enrutadores, modems y balanceo de carga.",
-        "precio" => 240.99,
-        "img" => "img/productos/redes/gestion_conexiones.jpg",
-        "categoria" => "Redes"
-    ],
-    [
-        "nombre" => "Seguridad",
-        "descripcion" => "Implementación de firewalls, sistemas de detección de intrusos (IDS), VPN (Red Privada Virtual) y políticas de seguridad para proteger los datos y la infraestructura de la red.",
-        "precio" => 750.99,
-        "img" => "img/productos/redes/seguridad_red.jpg",
-        "categoria" => "Redes"
-    ],
-    [
-        "nombre" => "Soporte técnico",
-        "descripcion" => "Diagnóstico y solución de problemas relacionados con la conectividad, lentitud de la red, o fallos en los dispositivos conectados a la red.",
-        "precio" => 450.99,
-        "img" => "img/productos/redes/soporte_tecnico.jpg",
-        "categoria" => "Redes"
-    ],
-    [
-        "nombre" => "Monitorización",
-        "descripcion" => "Vigilancia constante de la infraestructura de la red para garantizar un rendimiento óptimo, detectando y solucionando problemas de manera proactiva.",
-        "precio" => 150.99,
-        "img" => "img/productos/redes/monitoreo_redes.jpg",
-        "categoria" => "Redes"
-    ],
-    [
-        "nombre" => "Mantenimiento",
-        "descripcion" => "Mantenimiento y gestión de servidores, incluyendo la configuración de servicios como DHCP, DNS y servidores de archivos.",
-        "precio" => 950.99,
-        "img" => "img/productos/redes/admin_servidores.jpg",
-        "categoria" => "Redes"
-    ],
-    [
-        "nombre" => "Data recovery",
-        "descripcion" => "Implementación de soluciones para el respaldo de información y procedimientos para la recuperación de datos en caso de fallos en la red.",
-        "precio" => 550.99,
-        "img" => "img/productos/redes/recup_datos.jpg",
-        "categoria" => "Redes"
-    ],
-    [
-        "nombre" => "Innovación",
-        "descripcion" => "Evaluación y adopción de nuevas tecnologías de red, como el IoT (Internet de las cosas), redes 5G, o software-defined networking (SDN), para mejorar la infraestructura de la red.",
-        "precio" => 1200.99,
-        "img" => "img/productos/redes/new_tech.jpg",
-        "categoria" => "Redes"
-    ],
-    [
-        "nombre" => "BubbleOps",
-        "descripcion" => "Sistema operativo innovador diseñado para entornos en la nube y sistemas distribuidos.",
-        "precio" => 49.99,
-        "img" => "img/productos/sistemas/bubbleops.jpg",
-        "categoria" => "Sistemas"
-    ],
-    [
-        "nombre" => "ChompOS",
-        "descripcion" => "Con una interfaz minimalista pero altamente configurable, ChompOS se adapta a servidores, dispositivos embebidos y estaciones de trabajo exigentes.",
-        "precio" => 55.00,
-        "img" => "img/productos/sistemas/chompos.jpg",
-        "categoria" => "Sistemas"
-    ],
-    [
-        "nombre" => "Computopia",
-        "descripcion" => "Sistema operativo futurista diseñado para ofrecer una experiencia de usuario completamente personalizada e intuitiva.",
-        "precio" => 97.50,
-        "img" => "img/productos/sistemas/computopia.jpg",
-        "categoria" => "Sistemas"
-    ],
-    [
-        "nombre" => "DreamPad",
-        "descripcion" => "Sistema operativo innovador diseñado para la creatividad y la productividad, combinando una interfaz inmersiva con herramientas avanzadas de diseño, escritura y multimedia.",
-        "precio" => 45.00,
-        "img" => "img/productos/sistemas/dreampad.jpg",
-        "categoria" => "Sistemas"
-    ],
-    [
-        "nombre" => "Euphoria",
-        "descripcion" => "Su interfaz fluida y sensorial se adapta dinámicamente a las preferencias del usuario, integrando realidad aumentada y controles gestuales para una interacción más natural.",
-        "precio" => 99.99,
-        "img" => "img/productos/sistemas/euphoria.jpg",
-        "categoria" => "Sistemas"
-    ],
-    [
-        "nombre" => "Funnix",
-        "descripcion" => "Con un fuerte enfoque en la seguridad y la creatividad, Funnix es ideal para estudiantes, creadores de contenido y cualquier persona que busque una experiencia digital más entretenida e intuitiva.",
-        "precio" => 34.99,
-        "img" => "img/productos/sistemas/funnix.jpg",
-        "categoria" => "Sistemas"
-    ],
-    [
-        "nombre" => "Happysys",
-        "descripcion" => "Con una interfaz amigable y colorida, incorpora elementos de bienestar digital, como recordatorios para pausas saludables, música relajante y modos de concentración inteligentes.",
-        "precio" => 59.99,
-        "img" => "img/productos/sistemas/happysys.jpg",
-        "categoria" => "Sistemas"
-    ],
-    [
-        "nombre" => "RedDwarf",
-        "descripcion" => "Sistema operativo ultraligero y optimizado para funcionar en entornos de bajo consumo y hardware compacto.",
-        "precio" => 85.50,
-        "img" => "img/productos/sistemas/reddwarf.jpg",
-        "categoria" => "Sistemas"
-    ],
-    [
-        "nombre" => "Runix",
-        "descripcion" => "Inspirado en Unix y Linux, combina la estabilidad de los sistemas tipo Unix con una interfaz intuitiva y moderna.",
-        "precio" => 89.99,
-        "img" => "img/productos/sistemas/runix.jpg",
-        "categoria" => "Sistemas"
-    ],    
-    [
-        "nombre" => "Wondrium",
-        "descripcion" => "Ideal para estudiantes, profesionales y cualquier persona interesada en explorar y expandir sus conocimientos de manera innovadora.",
-        "precio" => 42.00,
-        "img" => "img/productos/sistemas/wondrium.jpg",
-        "categoria" => "Sistemas"
+        'nombre' => 'Cisco CBS250-24T-4X',
+        'descripcion' => 'La serie Cisco Business 250 es la próxima generación de conmutadores inteligentes asequibles que combinan un potente rendimiento de red, seguridad y confiabilidad con un conjunto completo de funciones de red.',
+        'precio' => 1140.59,
+        'img' => 'img/productos/switches/9.webp',
+        'categoria' => 'Switches'
     ]
 ];
 
