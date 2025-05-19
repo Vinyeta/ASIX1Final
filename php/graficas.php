9bb9f22a8d19207bd886c4c2881535643554a5a7
Oscar0189 <ofmb89@gmail.com>
2025-05-19 12:47:30 +0200

    Update graficas.php

--- php/graficas.php
+++ php/graficas.php
@@ -1,5 +1,9 @@
 <?php
     session_start();
+    session_start();
+    if(!isset($_SESSION['id'])){
+        header("Location: index.php");
+    }
     require_once("header.php");
     require_once("conexionok.php");
 ?>
